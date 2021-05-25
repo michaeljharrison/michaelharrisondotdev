9ea1626b048c233c61a1afff735857d3ff635954
Mike <Michael.J.Harrison@outlook.com>
2021-05-25 15:29:29 +1000

    Update E2E tests (remove them)

--- test/e2e/specs/index.spec.js
+++ test/e2e/specs/index.spec.js
@@ -2,8 +2,6 @@ describe('Example test', () => {
   test('open app correctly', (browser) => {
     const main = browser.page.main()
     main.navigate()
-    main.assert.visible('@docButton')
-    main.assert.visible('@githubButton')
     browser.end()
   })
 })
